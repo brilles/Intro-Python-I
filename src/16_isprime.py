2d7c593067316e71a7668d0832972d625b46c964
Brian Illes <brianrilles@gmail.com>
2019-05-28 15:17:56 -0700

    Stretch: is prime program complete

--- src/16_isprime.py
+++ src/16_isprime.py
@@ -5,11 +5,12 @@ def isPrime(num):
         num = int(num)
         numList = [i for i in range(2,num)]
         print(numList)
+
         for number in numList:
             if num % number == 0:
                 return f"{num} is NOT prime, its divisible by (at least) {number}"
         else:
-            return "its prime!"
+            return f"{num} is prime!"
     else: 
         return "Incorrect usage, number must be greater that 0"
 
