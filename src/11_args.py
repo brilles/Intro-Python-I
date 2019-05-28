e614900021e232869d979927567196e00d791e97
Brian Illes <brianrilles@gmail.com>
2019-05-28 14:36:19 -0700

    MVP complete

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(int1, int2):
+    return int1 + int2
 
 print(f1(1, 2))
 
@@ -12,6 +14,15 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    return sum(args)
+
+# second solution without built-in sum fn
+# def f2(*args):
+#     count = 0
+#     for num in args:
+#         count += num
+#     return count    
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +32,16 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
+# what that is doing is expanding the iterable (list) w/ in the fn call 
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num1, num2=1):
+    return num1 + num2
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +56,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kargs): # ** allows for any num of keyword args and store them in a dict called kargs
+    for key, value in kargs.items():
+        print("key: " + str(key) + "," + " value: " + str(value))
 
 # Should print
 # key: a, value: 12
@@ -60,4 +77,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,23 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+def calendarOutput():
+  # default args are *now*
+  def printCal(month =datetime.now().month, year =datetime.now().year):
+    print(calendar.TextCalendar().formatmonth(int(year), int(month)))
+
+  # if 0 arg 
+  if len(sys.argv) == 1: 
+    printCal();
+  # if 1 arg
+  elif len(sys.argv) == 2:
+    printCal(sys.argv[1])
+  # if 2 args
+  elif len(sys.argv) == 3: 
+    printCal(sys.argv[1], sys.argv[2][1:-1])
+  # otherwise if > 2 args
+  else:
+    print("\n***\nIncorrect usage, please use command form <calendar.py month [year]>\n***")
+
+calendarOutput();
